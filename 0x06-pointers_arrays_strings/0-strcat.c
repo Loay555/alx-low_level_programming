7f03208e86be6e7619df0cc55b222fe924ebe265
Loay Ayman <loayyaymann@gmail.com>
2023-08-23 06:23:02 -0700

    zig

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,28 @@
+#include "main.h"]
+/**
+ * _strcat - concatenates 2 strings
+ * @dest: input value
+ * @src: input value
+ * Return: void
+ */
+char *_strcat(char *dest, char *src)
+{
+	int u;
+	int p;
+
+	u = 0;
+	while (dest[u] != '\0')
+	{
+		u++;
+	}
+	p = 0;
+	while (src[p] != '\0')
+	{
+		dest[u] = src[p];
+		u++;
+		p++;
+	}
+
+	dest[u] = '\0';
+	return (dest);
+}
