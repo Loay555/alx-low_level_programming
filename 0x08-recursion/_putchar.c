d5ce8c153d55de370d810478af7e815686aa2713
Loay Ayman <loayyaymann@gmail.com>
2023-08-30 07:13:09 -0700

    tera

---
+++ 0x08-recursion/_putchar.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <unistd.h>
+
+/**
+ * _putchar - Writes character c to stdout
+ * @c: the character to print
+ *
+ * Return: 1 (on success)
+ * On error return -1 and errno is set appropriately
+ */
+
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+
+}
