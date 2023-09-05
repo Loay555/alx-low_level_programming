a98733aea985d8fecc77f525d793ec3dedd9cd83
Loay Ayman <loayyaymann@gmail.com>
2023-09-05 16:32:32 -0700

    tag

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,42 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ *str_concat - Get ends of input and add together for size
+ *@s1: input 1 to concat
+ *@s2: input 2 to concat
+ *Return: concat of s1 and s2
+ */
+
+char *str_concat(char *s1, char *s2)
+{
+	char *doc;
+	int web, site;
+
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
+		web = site = 0;
+	while (s1[web] != '\0')
+		web++;
+	while (s2[site] != '\0')
+		site++;
+	doc = malloc(sizeof(char) * (web + site + 1));
+	if (doc == NULL)
+		return (NULL);
+	web = site = 0;
+	while (s1[web] != '\0')
+	{
+		doc[web] = s1[web];
+		web++;
+	}
+	while (s2[site] != '\0')
+	{
+		doc[web] = s2[site];
+		web++, site++;
+	}
+	doc[web] = '\0';
+	return (doc);
+
+}
