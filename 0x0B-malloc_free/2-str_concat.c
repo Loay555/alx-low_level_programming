0ce35c4a739225777d2ca4035d99e178d1bd6d9b
Loay Ayman <loayyaymann@gmail.com>
2023-09-05 17:26:19 -0700

    tag

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -10,7 +10,7 @@
 
 char *str_concat(char *s1, char *s2)
 {
-	char *doc;
+	char *conct;
 	int w, e;
 
 	if (s1 == NULL)
@@ -22,21 +22,21 @@ char *str_concat(char *s1, char *s2)
 		w++;
 	while (s2[e] != '\0')
 		e++;
-	doc = malloc(sizeof(char) * (w + e + 1));
-	if (doc == NULL)
+	conct = malloc(sizeof(char) * (w + e + 1));
+	if (conct == NULL)
 		return (NULL);
 	w = e = 0;
 	while (s1[w] != '\0')
 	{
-		doc[w] = s1[w];
+		conct[w] = s1[w];
 		w++;
 	}
 	while (s2[e] != '\0')
 	{
-		doc[w] = s2[e];
+		conct[w] = s2[e];
 		w++, e++;
 	}
-		doc[w] = '\0';
-		return (doc);
+	conct[w] = '\0';
+	return (conct);
 
 }
