c95981140d3dac121fbe9f4c3804106e99cec1b5
Loay Ayman <loayyaymann@gmail.com>
2023-09-05 19:12:01 -0700

    tag

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -3,40 +3,44 @@
 
 /**
  *str_concat - Get ends of input and add together for size
- *@s1: input 1 to concat
- *@s2: input 2 to concat
+ *@s1: input number 1 to concat
+ *@s2: input number 2 to concat
  *Return: concat of s1 and s2
  */
 
 char *str_concat(char *s1, char *s2)
 {
 	char *doc;
-	int w, e;
+	int a, b;
 
 	if (s1 == NULL)
 		s1 = "";
 	if (s2 == NULL)
 		s2 = "";
-	i = e = 0;
-	while (s1[w] != '\0')
-		w++;
-	while (s2[e] != '\0')
-		e++;
-	doc = malloc(sizeof(char) * (w + e + 1));
+		a = b = 0;
+	while (s1[a] != '\0')
+		a++;
+	while (s2[b] != '\0')
+		b++;
+
+	doc = malloc(sizeof(char) * (a + b + 1));
+
 	if (doc == NULL)
 		return (NULL);
-	w = e = 0;
-	while (s1[w] != '\0')
+
+	a = b = 0;
+
+	while (s1[a] != '\0')
 	{
-		doc[w] = s1[w];
-		w++;
+		doc[a] = s1[a];
+		a++;
 	}
-	while (s2[e] != '\0')
+	while (s2[b] != '\0')
 	{
-		doc[w] = s2[e];
-		w++, e++;
+		doc[a] = s2[b];
+		a++, b++;
 	}
-	        doc[w] = '\0';
+	doc[a] = '\0';
 	return (doc);
 
 }
