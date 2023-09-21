f2651ad0555a52cf352286f332b5b40c138282a5
Loay Ayman <loayyaymann@gmail.com>
2023-09-21 16:55:46 -0700

    green

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -10,17 +10,17 @@
 
 size_t print_list(const list_t *h)
 {
-	size_t e = 0;
+	size_t s = 0;
 
 	while (h)
 	{
 		if (!h->str)
 			printf("[0] (nil)\n");
 		else
-			printf("[%u] %e\n", h->len, h->str);
+			printf("[%u] %s\n", h->len, h->str);
 		h = h->next;
-		e++;
+		s++;
 	}
-	return (e);
+	return (s);
 
 }
